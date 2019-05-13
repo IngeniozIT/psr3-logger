68ed2a97281e7c9e474f5bc33ab7545206cdf46d
Heru-Luin <4009826+Heru-Luin@users.noreply.github.com>
2019-05-13 16:05:23 +0200

    Add files

---
+++ README.md
@@ -0,0 +1,8 @@
+# PSR-3 : Logger
+
+Implementation of the PSR-3 : LoggerInterface.
+
+[PSR-3 documentation](https://www.php-fig.org/psr/psr-3/)
+
+[![Build Status](https://travis-ci.com/IngeniozIT/psr3-logger.svg?branch=master)](https://travis-ci.com/IngeniozIT/psr3-logger)
+[![Code Coverage](https://codecov.io/gh/IngeniozIT/psr3-logger/branch/master/graph/badge.svg)](https://codecov.io/gh/IngeniozIT/psr3-logger)
---
+++ src/Logger.php
@@ -0,0 +1,201 @@
+<?php
+declare(strict_types = 1);
+
+namespace IngeniozIT\Psr3;
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+use Psr\Log\InvalidArgumentException;
+
+class Logger implements LoggerInterface
+{
+    protected $handle;
+
+    /**
+     * Constructor.
+     *
+     * @param string $file The file where the logs will be written.
+     *
+     * @return self
+     */
+    public function __construct(string $file = 'php://stderr')
+    {
+        $this->handle = fopen($file, 'a');
+    }
+
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+        fclose($this->handle);
+    }
+
+    /**
+     * Interpolates context values into the message placeholders.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return string
+     */
+    protected function interpolate($message, array $context = []): string
+    {
+        // build a replacement array with braces around the context keys
+        $replace = array();
+        foreach ($context as $key => $val) {
+            // check that the value can be casted to string
+            if (!is_array($val) && (!is_object($val) || method_exists($val, '__toString'))) {
+                $replace['{' . $key . '}'] = $val;
+            }
+        }
+
+        // interpolate replacement values into the message and return
+        return strtr((string)$message, $replace);
+    }
+
+    /**
+     * System is unusable.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function emergency($message, array $context = []): void
+    {
+        $this->log(LogLevel::EMERGENCY, $message, $context);
+    }
+
+    /**
+     * Action must be taken immediately.
+     *
+     * Example: Entire website down, database unavailable, etc. This should
+     * trigger the SMS alerts and wake you up.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function alert($message, array $context = []): void
+    {
+        $this->log(LogLevel::ALERT, $message, $context);
+    }
+
+    /**
+     * Critical conditions.
+     *
+     * Example: Application component unavailable, unexpected exception.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function critical($message, array $context = []): void
+    {
+        $this->log(LogLevel::CRITICAL, $message, $context);
+    }
+
+    /**
+     * Runtime errors that do not require immediate action but should typically
+     * be logged and monitored.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function error($message, array $context = []): void
+    {
+        $this->log(LogLevel::ERROR, $message, $context);
+    }
+
+    /**
+     * Exceptional occurrences that are not errors.
+     *
+     * Example: Use of deprecated APIs, poor use of an API, undesirable things
+     * that are not necessarily wrong.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function warning($message, array $context = []): void
+    {
+        $this->log(LogLevel::WARNING, $message, $context);
+    }
+
+    /**
+     * Normal but significant events.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function notice($message, array $context = []): void
+    {
+        $this->log(LogLevel::NOTICE, $message, $context);
+    }
+
+    /**
+     * Interesting events.
+     *
+     * Example: User logs in, SQL logs.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function info($message, array $context = []): void
+    {
+        $this->log(LogLevel::INFO, $message, $context);
+    }
+
+    /**
+     * Detailed debug information.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function debug($message, array $context = []): void
+    {
+        $this->log(LogLevel::DEBUG, $message, $context);
+    }
+
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed  $level
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     * @throws InvalidArgumentException
+     */
+    public function log($level, $message, array $context = []): void
+    {
+        $levels = [
+            LogLevel::EMERGENCY => true,
+            LogLevel::ALERT => true,
+            LogLevel::CRITICAL => true,
+            LogLevel::ERROR => true,
+            LogLevel::WARNING => true,
+            LogLevel::NOTICE => true,
+            LogLevel::INFO => true,
+            LogLevel::DEBUG => true,
+        ];
+
+        if (!isset($levels[$level])) {
+            throw new InvalidArgumentException('Invalid log level.');
+        }
+
+        fwrite($this->handle, $level.' '.$this->interpolate($message, $context)."\n");
+    }
+}
